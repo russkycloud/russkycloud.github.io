68126d44934a253a6367726c39faa1768a98e806
russkycloud <51924104+russkycloud@users.noreply.github.com>
2019-07-24 10:24:33 +0600

    12

--- face-api/faces.js
+++ face-api/faces.js
@@ -1,1 +1,18 @@
 const video = document.getElementById('video')
+
+//Loading the Model Data
+const MODEL_URL = '/weights'
+
+await faceapi.loadSsdMobilenetv1Model(MODEL_URL)
+await faceapi.loadFaceLandmarkModel(MODEL_URL)
+await faceapi.loadFaceRecognitionModel(MODEL_URL)
+
+//Receive a Full Description for all Faces from an Input Image
+const input = document.getElementById('imageUpload')
+let fullFaceDescriptions = await faceapi.detectAllFaces(input).withFaceLandmarks().withFaceDescriptors()
+
+//original image size you can simply resize them
+fullFaceDescriptions = faceapi.resizeResults(fullFaceDescriptions)
+
+//visualize the detection results by drawing the bounding boxes into a canvas:
+faceapi.draw.drawDetections(canvas, fullFaceDescriptions)
